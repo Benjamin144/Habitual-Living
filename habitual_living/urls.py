46490ecc5f1afdda09bd99ae84552ea26699769f
Benjamin144 <josephroberts@email.com>
2021-03-23 22:43:54 +0000

    adjustments and implementation to ceckout folder

--- habitual_living/urls.py
+++ habitual_living/urls.py
@@ -17,14 +17,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blog.views import frontend, post_detail
-
 urlpatterns = [
-    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
-    path('<slug:slug>/', post_detail, name='post_detail'),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

13257bd63f828b95f0ee3b5467d31b69144099a7
Benjamin144 <josephroberts@email.com>
2021-03-23 09:22:18 +0000

    additions to blog view.py

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ posts = Post.objects.all()
 
 def frontend(request):
     return render(request, 'blog/frontend.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    return render(request, 'blog/post_detail.html', {'post': post})

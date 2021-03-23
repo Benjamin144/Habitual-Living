d2473ca06099942e1907075012fe1eee41414ad1
Benjamin144 <josephroberts@email.com>
2021-03-23 08:43:27 +0000

    revisions to blog template and py files

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from .models import Post
+posts = Post.objects.all()
+
 def frontend(request):
-    return render(request, 'blog/frontend.html', {})
+    return render(request, 'blog/frontend.html', {'posts': posts})
--- habitual_living/urls.py
+++ habitual_living/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 from blog.views import frontend
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),

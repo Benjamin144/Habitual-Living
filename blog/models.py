bce3111d3edc217c23eda5ff5c7414d695d919c9
Benjamin144 <josephroberts@email.com>
2021-03-23 10:15:30 +0000

    created new model comments in blog

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,17 @@ class Post(models.Model):
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
-    class ordering:
+    class Meta:
 
         ordering = ['-date_added']
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = ['date_added']

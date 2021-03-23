adaef9a2b297c27160e6f91a648bafc2b5e877ac
Benjamin144 <josephroberts@email.com>
2021-03-23 18:31:08 +0000

    added text box for comment field

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Post(models.Model):
+
+    
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -12,6 +14,8 @@ class Post(models.Model):
         ordering = ['-date_added']
 
 class Comment(models.Model):
+
+
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField()
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import CommentForm
 from .models import Post
-
 
 def frontend(request):
     posts = Post.objects.all()
@@ -11,4 +11,16 @@ def frontend(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
 
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
